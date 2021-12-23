44270a22f76bc2b81971751faf0f33bf00e4e9dd
laptop <juha.jylhakoski@aalto.fi>
2021-12-23 08:19:37 +0800

    13.10

---
+++ 13.10/blogs/backend/controllers/users.js
@@ -0,0 +1,116 @@
+const bcrypt = require('bcrypt')
+
+const usersRouter = require('express').Router()
+
+const { User } = require('../models')
+
+//const User = require('../models/user')
+
+const { check, validationResult } = require('express-validator')
+
+usersRouter.get('/users', async (request, response) => {
+  const users = await User
+    .find({})
+    .populate('blogs', { title: 1, url: 1, likes: 1, author: 1 })
+
+  response.json(users.map(user => user.toJSON()))
+})
+
+usersRouter.get('/users/:id', async (request, response) => {
+
+  try {
+
+    const user = await User.findByPk(request.params.id)
+    
+    if (user) {
+
+      response.json(user)
+
+    } else {
+
+      response.status(404).end()
+    }
+
+  } catch (error) {
+
+    console.error(error);
+  }
+})
+
+usersRouter.post('/users', [check('username').isEmail()], async (request, response) => {
+
+  const { username, name, password } = request.body
+
+  console.log('users', username, name, password)
+
+  const errors = validationResult(request)
+
+  if (!errors.isEmpty()) {
+
+    console.log(JSON.stringify({errors: errors.array()}))
+
+    return response.status(400).json({errors: errors.array()})
+  }
+
+  if ( !password || password.length < 3 ) {
+
+    return response.status(400).send({
+      error: 'Error: password must min length 3'
+    })
+  }
+
+  try {
+
+    const saltRounds = 10
+
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+      username, 
+      name,
+      passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.json(savedUser)
+
+  } catch (error) {
+
+    console.error(error)
+
+    return response.status(400).send({
+      error: 'Validation isEmail on username failed.'
+    })
+  }
+
+})
+
+usersRouter.put('/users/:username', async (request, response) => {
+
+  console.log('request.params.username', request.params.username)
+
+  try {
+
+    const user = await User.findOne({ where: { username: request.params.username }})
+    
+    if (user) {
+
+      user.username = request.body.username
+
+      await user.save()
+
+      response.json(user)
+
+    } else {
+
+      response.status(404).end()
+    }
+
+  } catch (error) {
+
+    console.error(error);
+  }
+})
+
+module.exports = usersRouter
